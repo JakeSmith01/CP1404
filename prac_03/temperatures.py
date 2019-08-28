41276758285aa9b51b0d84b9fc181200e268f0c0
JakeSmith01 <jake.smith@my.jcu.edu.au>
2019-08-28 22:17:54 +1000

    fix functions

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -1,25 +1,27 @@
-def main():
+def main(converted_celsius, converted_fahrenheit):
     print('Temperature converter')
     choice = input('Convert from (C)elsius or (F)ahrenheit:').upper()
     if choice == 'C':
         celsius_temp = int(input('Please enter a temperature in celsius'))
         convert_celsius(celsius_temp)
-        print('This converts to', converted_temp, 'Fahrenheit')
+        print('This converts to', converted_celsius, 'Fahrenheit')
     elif choice == 'F':
         fahrenheit_temp = int(input('Please enter a temperature in fahrenheit'))
         convert_fahrenheit(fahrenheit_temp)
-        print('This converts to', converted_temp, 'Celsius')
+        print('This converts to', converted_fahrenheit, 'Celsius')
     else:
         print('Invalid Choice')
 
 
 def convert_celsius(celsius_temp):
-    converted_temp = (celsius_temp * (9/5)) + 32
-    return converted_temp
+    converted_celsius = (celsius_temp * (9/5)) + 32
+    return converted_celsius
 
 
 def convert_fahrenheit(fahrenheit_temp):
-    converted_temp = ((fahrenheit_temp - 32) / (5/9))
-    return converted_temp
+    converted_fahrenheit = ((fahrenheit_temp - 32) / (5/9))
+    return converted_fahrenheit
+
 
 main()
+
