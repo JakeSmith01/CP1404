c15572f7d2f03d69191ff1735d3704304075ff50
JakeSmith01 <jake.smith@my.jcu.edu.au>
2019-08-29 11:11:15 +1000

    add print statements

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,6 +8,6 @@ print('The first number is {}'.format(numbers[0]))
 print('The last number is {}'.format(numbers[-1]))
 print('The smallest number is {}'.format(min(numbers)))
 print('The largest number is {}'.format(max(numbers)))
-print('The average of those numbers is {}'.format(sum(numbers)))
+print('The average of those numbers is {}'.format(sum(numbers)/len(numbers)))
 
 
