8c2337847f8438ee55d58cef03e30df9a25d4f00
JakeSmith01 <jake.smith@my.jcu.edu.au>
2019-08-29 10:34:37 +1000

    add comments

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,2 +1,25 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
+# numbers[0]= ten
+# numbers[-1] = 1
+# numbers[2:]
+# 9 in numbers
+
+# numbers[0]
+# This will display the first number in the list which is 3,
+# numbers[-1]
+# This will display the last number in the list, in this case 2.
+# numbers[3]
+# This will display the number 1
+# numbers[:-1]
+# This displays all numbers but the last
+#  numbers[3:4]
+# This displays the numbers between the fourth and fifth item in the list which is only 1.
+#  5 in numbers
+# Declares true if 5 is in numbers. Declares true
+#  7 in numbers
+# Declares true if 7 is in numbers. Declares false
+# "3" in numbers
+# Asks if the string 3 is in numbers. It isnt so declares false
+# numbers + [6, 5, 3]
+# Adds 6,5 and 3 to the list.
