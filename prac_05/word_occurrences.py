b6b98f5da5eb5efd42c3ddc25147b6395ba387b9
JakeSmith01 <jake.smith@my.jcu.edu.au>
2019-09-10 15:08:02 +1000

    add word_occurrences

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,16 @@
+words_in_string = {}
+string = input('Text:')
+words = string.split()
+
+for word in words:
+    try:
+        words_in_string[word] += 1
+    except KeyError:
+        words_in_string[word] = 1
+
+words = list(words_in_string.keys())
+words.sort()
+
+words_length = max((len(word) for word in words))
+for word in words:
+    print('{:{}}:{}'.format(word,words_length, words_in_string[word]))
