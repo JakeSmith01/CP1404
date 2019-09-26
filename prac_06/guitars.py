f2f5309c6444236524f38b55485d9c4ff431cfe9
JakeSmith01 <jake.smith@my.jcu.edu.au>
2019-09-26 11:25:22 +1000

    add box_layout.kv and box_layout_demo.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,7 +15,7 @@ while name != '':
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-
+print('These are my guitars:')
 for i, guitar in enumerate(guitars):
     if guitar.is_vintage():
         vintage_string = '(Vintage)'
